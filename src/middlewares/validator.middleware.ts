ce18ae8002a3de285c274904ec192820addabfe6
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-01-25 22:22:20 +0530

    added eslint

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,16 +1,17 @@
-import { Validator } from 'node-input-validator';
-import { NextFunction, Request, Response } from '../types';
+import { Validator } from 'node-input-validator'
+import { NextFunction, Request, Response } from '../types'
 
 export const validate = (schema: object, source: 'body' | 'query' = 'body') => {
+  console.log(source)
   return async (request: Request, response: Response, next: NextFunction) => {
-    const v = new Validator(request.body, schema);
-    const matched = await v.check();
+    const v = new Validator(request.body as object, schema)
+    const matched = await v.check()
     if (!matched) {
       return response.status(422).send({
         data: null,
         errors: v.errors,
-      });
+      })
     }
-    next();
-  };
-};
+    next()
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -1,72 +1,73 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   Request as ExpressRequest,
   Response as ExpressResponse,
   NextFunction as ExpressNextFunction,
   Express as Ex,
   Router as Rt,
-} from 'express';
+} from 'express'
 
 // Interfaces
-export interface IUser {
-  firstName?: string;
-  lastName?: string;
-  email: string;
-  password: string;
+export type IUser = {
+  firstName?: string
+  lastName?: string
+  email: string
+  password: string
 }
 
-export interface IAdmin {
-  firstName?: string;
-  lastName?: string;
-  email: string;
-  password: string;
+export type IAdmin = {
+  firstName?: string
+  lastName?: string
+  email: string
+  password: string
 }
 
-export interface IProduct {
-  name: string;
-  description: string;
-  images: string[];
-  price: number;
+export type IProduct = {
+  name: string
+  description: string
+  images: string[]
+  price: number
 }
 
-export interface OrderItem {
-  product: string; // product id
-  price: number; // product price at the time of purchase
-  quantity: number;
+export type OrderItem = {
+  product: string // product id
+  price: number // product price at the time of purchase
+  quantity: number
 }
 
-export interface IOrder {
-  orderId: string; // order id;
-  user: string; // user id;
-  items: OrderItem[];
-  total: number;
-  orderDate: string;
+export type IOrder = {
+  orderId: string // order id;
+  user: string // user id;
+  items: OrderItem[]
+  total: number
+  orderDate: string
 }
 
-export interface IReview {
-  description: string;
-  user: string;
-  product: string;
-  rating: number;
+export type IReview = {
+  description: string
+  user: string
+  product: string
+  rating: number
 }
 
 // types
-export interface ITokenData {
-  id: string;
+export type ITokenData = {
+  id: string
 }
 export type ResponseType<T = any> = {
-  data: T;
-  errors: any;
-};
-export type Express = Ex;
-export type Router = Rt;
+  data: T
+  errors: any
+}
+export type Express = Ex
+export type Router = Rt
 export type Request<Body = any, Params = any, Query = any> = ExpressRequest<
   Params,
-  {},
+  any,
   Body,
   Query
-> & { user?: ITokenData };
-export type Response<DataType = any> = ExpressResponse<ResponseType<DataType>>;
-export type NextFunction = ExpressNextFunction;
+> & { user?: ITokenData }
+export type Response<DataType = any> = ExpressResponse<ResponseType<DataType>>
+export type NextFunction = ExpressNextFunction
 export enum AllowedUsers {
   ENDUSER = 'USER',
   ADMIN = 'ADMIN',
@@ -75,9 +76,9 @@ export type Controller<> = (
   request: Request,
   response: Response,
   next?: NextFunction,
-) => Response;
+) => Response
 export type Middleware = (
   request: Request,
   response: Response,
   next?: NextFunction,
-) => Response | void;
+) => Response | void
